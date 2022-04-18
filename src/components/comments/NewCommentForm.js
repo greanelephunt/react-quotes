10d56d329e435ea41200cf68f5c23c79e1c99bcb
Ivan Epihin <greanelephunt@gmail.com>
2022-04-18 21:12:55 +0300

    initial commit

---
+++ src/components/comments/NewCommentForm.js
@@ -0,0 +1,45 @@
+import { useRef, useEffect } from "react";
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
+
+import classes from "./NewCommentForm.module.css";
+
+const NewCommentForm = (props) => {
+  const commentTextRef = useRef();
+  const { sendRequest, status, error } = useHttp(addComment);
+  const {onCommentAdded, quoteId} = props;
+
+  useEffect(() => {
+    if(status === "completed" && !error) {
+      onCommentAdded();
+    }
+  }, [status, error, onCommentAdded]);
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    const commentText = commentTextRef.current.value;
+    // optional: Could validate here
+    sendRequest({commentData: { text: commentText }, quoteId});
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === "pending" && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
+      <div className={classes.control} onSubmit={submitFormHandler}>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
+      </div>
+      <div className={classes.actions}>
+        <button className="btn">Add Comment</button>
+      </div>
+    </form>
+  );
+};
+
+export default NewCommentForm;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="centered">
+      <p>Page not found ._.</p>
+    </div>
+  );
+};
+
+export default NotFound;
